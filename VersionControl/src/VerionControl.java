0db5d46842cf7115f962addb236e6e7e50bb698b
Masroor-shah12 <shah_sherazi12@yahoo.com>
2016-04-25 17:41:34 +0500

    My First Commit

---
+++ VersionControl/src/VerionControl.java
@@ -0,0 +1,10 @@
+
+public class VerionControl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello My Future");
+System.out.println("Hi Git hub");
+	}
+
+}
